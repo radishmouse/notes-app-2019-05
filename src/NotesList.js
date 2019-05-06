af05c2ca7dcd56f7c3df59081571c8c0e5db3a97
chris aquino <chris@radishmouse.com>
2019-05-06 10:38:35 -0400

    adds notes about danger of allowing parent to style

--- src/NotesList.js
+++ src/NotesList.js
@@ -2,6 +2,12 @@ import React from 'react';
 import styles from './NotesList.module.css';
 
 export default function NotesList(props) {
+
+    // Note: allowing styling from the parent
+    // may have unintended consequences.
+    // Parent styles may take precedence, which
+    // goes against per-component styling.
+
     // how do we combine styles?
     // console.log(props.className);
     // console.log(styles.list);
