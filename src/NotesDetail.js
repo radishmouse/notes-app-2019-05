155e090ab24ec721fc150df168b3321da8596502
chris aquino <chris@radishmouse.com>
2019-05-06 11:42:39 -0400

    changes notesdetail to a class

--- src/NotesDetail.js
+++ src/NotesDetail.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-export default function NotesDetail({className, note}) {
-    return (
-        <div className={className}>
-            {note.text}
-        </div>
-    );
+export default class NotesDetail extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        // declares the className and note variables
+        // and assigns them to the properties from this.props
+        // where the name matches.
+        const {className, note} = this.props;
+        return (
+            <div className={className}>
+                {note.text}
+            </div>
+        );
+    }
 }
