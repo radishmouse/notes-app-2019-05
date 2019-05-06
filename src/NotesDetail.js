35463c9cdbd72f13b877c67c0f27386c1d804605
chris aquino <chris@radishmouse.com>
2019-05-06 12:08:38 -0400

    allows editing of draft text

--- src/NotesDetail.js
+++ src/NotesDetail.js
@@ -1,8 +1,10 @@
 import React from 'react';
 
-function NotesEditor({text}) {
+function NotesEditor({text, handleChange}) {
     return (
-        <textarea value={text} />
+        <textarea value={text} onChange={(e) => {
+            handleChange(e.target.value);
+        }} />
     );
 }
 
@@ -10,7 +12,8 @@ export default class NotesDetail extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            isEditing: false
+            isEditing: false,
+            draftText: props.note.text
         };
     }
     render() {
@@ -18,13 +21,29 @@ export default class NotesDetail extends React.Component {
         // and assigns them to the properties from this.props
         // where the name matches.
         const {className, note} = this.props;
-        const {isEditing} = this.state;
+        const {isEditing, draftText} = this.state;
         return (
             <div className={className}>
                 {
-                    isEditing ? <NotesEditor text={note.text} /> : note.text
-                }                
+                    isEditing ? <NotesEditor 
+                                    handleChange={this._changeDraftText} 
+                                    text={draftText} 
+                                /> 
+                            : draftText
+                }
+                <br />
+                <button onClick={this._toggleIsEditing}>Toggle</button>                
             </div>
         );
+    }
+    _changeDraftText = (newText) => {
+        this.setState({
+            draftText: newText
+        });
+    }
+    _toggleIsEditing = () => {
+        this.setState({
+            isEditing: !this.state.isEditing
+        });
     }
 }
